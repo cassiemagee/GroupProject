ed33f1dd3515f341bf8827d21f094e41df21dd4f
nadu2012 <nadu303@gmail.com>
2016-10-17 16:47:39 +0100

    items

---
+++ items2.py
@@ -0,0 +1,44 @@
+item_hammer = {
+    "id": "hammer",
+
+    "name": "Hammer",
+
+    "description":
+    "after negotiation with Red, you got the hammer!!, a hammer is \n six or seven inches long, it could take someone"
+    
+}
+
+item_poster = {
+    "id": "poster"
+
+    "name": "Poster"
+
+    "description":
+    "the poster from the Warden's office, this would look great on my cell wall!!" 
+}
+
+item_Cloth = {
+    "id": "cloth",
+
+    "name": "Cloth",
+
+    "description":
+    "This Cloth is for polishing small rocks, and can be used with the rock Hammer \n to create items such as a chess set"
+
+}
+
+item_locket = {
+    "id": "locket",
+
+    "name": "Locket",
+
+    "description": "This is my wifes Locket, has no meaning to me other than monetary "
+}
+
+item_shank = {
+    "id": "shank",
+
+    "name": "Shank",
+
+    "description": "deadl "
+}
