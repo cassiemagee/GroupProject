18d9542b2e64be18947cbc04ddc76af542c73fb1
Nicholas Anastasiou <anastasiou.nicholas@gmail.com>
2016-10-18 09:25:39 +0100

    Add files via upload

---
+++ people.py
@@ -0,0 +1,27 @@
+people_sisters = {
+    "id": "sisters",
+    
+    "name" : "The Sisters",
+
+    "description":
+    """These are the Sisters who have a high reputation within the prison. """
+
+}
+
+people_guard = {
+    "id" : "guard",
+
+    "name" : "The Guard",
+
+    "description":
+    """The Guard mostly stays within the Guard's Office."""
+}
+
+people_red = {
+    "id" : "red",
+
+    "name" : "Red",
+
+    "description" :
+    """Red is the man in the prison who can get you things you need."""
+}
